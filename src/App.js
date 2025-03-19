1c828f808cecfb1b056a649c0b55b3709544dd31
SasinduNanayakkara <it20625016@my.sliit.lk>
2025-03-19 08:57:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageRoutes from './routes/PageRoutes';
+
+function App() {
+  return (
+    <div className="App">
+      <PageRoutes />
+    </div>
+  );
+}
+
+export default App;
